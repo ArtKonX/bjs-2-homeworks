07292dc3f4e7b859901d98532c445788990369df
Artemij Kondurov <kondurovartemij@yandex.ru>
2024-02-05 14:45:41 +0300

    The first task is completely ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+
+	let arr;
+
+	let discriminant = b ** 2 - 4 * a * c;
+
+	if (discriminant === 0) {
+		arr = [];
+		let root = -b / (2 * a);
+		arr.push(Number(root.toFixed(2)));
+	} else if (discriminant > 0) {
+		arr = [];
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(Number(root1.toFixed(2)), Number(root2.toFixed(2)));
+	} else {
+		arr = [];
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let monthlyInterestRate = percent / 100 / 12;
+	let loanBody = amount - contribution;
+	let payment = loanBody * (monthlyInterestRate + (monthlyInterestRate / (((1 + monthlyInterestRate) ** countMonths) - 1)));
+	let totalAmount = Number((payment * countMonths).toFixed(2));
+	return totalAmount;
 }
